f14b68c8819cac27dd0d9a10c4741fd93c2c759f
YewSing <23004976@siswa.um.edu.my>
2025-08-04 22:59:29 +0800

    Add project files.

---
+++ ExpenseTracker/Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTracker.Models
+{
+    public class Transaction
+    {
+        [Key]   //Primary Key
+        public int TranscationId { get; set; }
+
+        //CategoryId as foreign key
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }    // navigational property
+        public int Amount { get; set; }
+
+        [Column(TypeName ="nvarchar(75)")]
+        public string? Note { get; set; }
+
+        public DateTime Date {  get; set; } = DateTime.Now;
+    }
+}
